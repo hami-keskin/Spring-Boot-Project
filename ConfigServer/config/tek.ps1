b31a87d8e55e1ada76771a4fd496b50e214d346b
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2025-01-08 20:37:48 +0300

    .

--- ConfigServer/config/tek.ps1
+++ ConfigServer/config/tek.ps1
@@ -2,12 +2,14 @@
 cd D:\WWW\Yazilim\spring\Spring\ConfigServer
 Start-Process cmd -ArgumentList "/k mvn spring-boot:run"
 
+Start-Sleep -Seconds 30
+
 # EurekaServer'ı çalıştırma komutu
 cd D:\WWW\Yazilim\spring\Spring\EurekaServer
 Start-Process cmd -ArgumentList "/k mvn spring-boot:run"
 
 # 20 saniye bekle
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 30
 
 # ProductService instance1'i çalıştırma komutu
 cd D:\WWW\Yazilim\spring\Spring\ProductService
